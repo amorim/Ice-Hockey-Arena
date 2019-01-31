e9c1906dfaa2a32bd17ea35a228e7db3857b9df6
NelsonGomesNeto <ngn@ic.ufal.br>
2019-01-31 16:16:54 -0300

    Adds draw line by line equation

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
-/*
- * Desafio: verificar o modelview
- */
-
+#if _WIN32
+  #ifndef _WIN32
+    #include <windows.h>
+  #endif
+#endif
 #include <iostream>
 #include <algorithm>
 using namespace std;
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <cmath>
 
 const int WIDTH = 500, HEIGHT = 800;
+const bool pixel = true, bresenhamMode = true;
+const double squareSizeDiv2 = 0.5;
 
 bool ready = false;
 
@@ -34,6 +37,12 @@ void init(void) {
     gluOrtho2D(0, WIDTH, 0, HEIGHT);
 }
 
+void drawPoint(double x, double y)
+{
+    if (pixel) glVertex2d(x, y);
+    else glRectd(x - squareSizeDiv2, y - squareSizeDiv2, x + squareSizeDiv2, y + squareSizeDiv2);
+}
+
 void bresenham(Line line)
 {
     glBegin(GL_POINTS);
@@ -49,31 +58,46 @@ void bresenham(Line line)
     d = 2 * dy - dx;
     E = 2 * dy, NE = 2 * (dy - dx);
     x = line.pi.x, y = line.pi.y;
-    if (steep) glVertex2d(y, x); else glVertex2d(x, y);
+    if (steep) drawPoint(y, x); else drawPoint(x, y);
     while (x < line.pf.x)
     {
         if (d <= 0) d += E, x ++;
         else d += NE, x ++, y += yStep;
 //        printf("(%d, %d)\n", x, y);
-        if (steep) glVertex2d(y, x); else glVertex2d(x, y);
+        if (steep) drawPoint(y, x); else drawPoint(x, y);
     }
     line = aux;
     glEnd();
 }
-
-void circlePoints(int x, int y, bool q1, bool q2, bool q3, bool q4)
+void lineEquation(Line line)
 {
-    if (q1) glVertex2d(x, y), glVertex2d(y, x);
-    if (q2) glVertex2d(-x, y), glVertex2d(-y, x);
-    if (q3) glVertex2d(-x, -y), glVertex2d(-y, -x);
-    if (q4) glVertex2d(y, -x), glVertex2d(x, -y);
+    // y = m*x + b
+    // m = (y1 - y0)/(x1 - x0)
+    // b = 0 (in this case)
+    bool steep = abs(line.pf.y - line.pi.y) > abs(line.pf.x - line.pi.x);
+    if (steep) swap(line.pi.x, line.pi.y), swap(line.pf.x, line.pf.y);
+    if (line.pi.x > line.pf.x) swap(line.pi.x, line.pf.x), swap(line.pi.y, line.pf.y);
+    double m = (line.pf.y - line.pi.y) / (line.pf.x - line.pi.x);
+    for (double x = line.pi.x, y = line.pi.y; x <= line.pf.x; x ++)
+    {
+        if (steep) drawPoint(y, x); else drawPoint(x, y);
+        y = m * x;
+    }
+}
+
+void circlePoints(double x, double y, bool q1, bool q2, bool q3, bool q4)
+{
+    if (q1) drawPoint(x, y), drawPoint(y, x);
+    if (q2) drawPoint(-x, y), drawPoint(-y, x);
+    if (q3) drawPoint(-x, -y), drawPoint(-y, -x);
+    if (q4) drawPoint(y, -x), drawPoint(x, -y);
 }
 void midPointCircle(Circle circle, bool q1, bool q2, bool q3, bool q4)
 {
     glPushMatrix();
     glTranslated(circle.center.x, circle.center.y, 0);
     glBegin(GL_POINTS);
-    int x = 0, y = (int) circle.radious;
+    int x = 0, y = circle.radious;
     double d;
     d = (double) 5 / 4 - circle.radious;
 
@@ -88,6 +112,17 @@ void midPointCircle(Circle circle, bool q1, bool q2, bool q3, bool q4)
     glPopMatrix();
 }
 
+void drawLine(Line line)
+{
+    if (bresenhamMode) bresenham(line);
+    else lineEquation(line);
+}
+void drawCircle(Circle circle, bool q1, bool q2, bool q3, bool q4)
+{
+    if (bresenhamMode) midPointCircle(circle, q1, q2, q3, q4);
+    else;
+}
+
 double rot = 0;
 int factor = 1;
 void display(void) {
@@ -96,17 +131,17 @@ void display(void) {
 
     if (ready) {
         glColor3ub(0, 0, 255);
-        bresenham({{startPoints[0].x + cornerRadious, startPoints[0].y}, {startPoints[1].x - cornerRadious, startPoints[0].y}});
-        bresenham({{startPoints[1].x, startPoints[0].y - cornerRadious}, {startPoints[1].x, startPoints[1].y + cornerRadious}});
-        bresenham({{startPoints[1].x - cornerRadious, startPoints[1].y}, {startPoints[0].x + cornerRadious, startPoints[1].y}});
-        bresenham({{startPoints[0].x, startPoints[1].y + cornerRadious}, {startPoints[0].x, startPoints[0].y - cornerRadious}});
+        drawLine({{startPoints[0].x + cornerRadious, startPoints[0].y}, {startPoints[1].x - cornerRadious, startPoints[0].y}});
+        drawLine({{startPoints[1].x, startPoints[0].y - cornerRadious}, {startPoints[1].x, startPoints[1].y + cornerRadious}});
+        drawLine({{startPoints[1].x - cornerRadious, startPoints[1].y}, {startPoints[0].x + cornerRadious, startPoints[1].y}});
+        drawLine({{startPoints[0].x, startPoints[1].y + cornerRadious}, {startPoints[0].x, startPoints[0].y - cornerRadious}});
 
         double displacement = cornerRadious;
 
-        midPointCircle({{startPoints[1].x - displacement, startPoints[0].y - displacement}, cornerRadious}, true, false, false, false);
-        midPointCircle({{startPoints[0].x + displacement, startPoints[0].y - displacement}, cornerRadious}, false, true, false, false);
-        midPointCircle({{startPoints[0].x + displacement, startPoints[1].y + displacement}, cornerRadious}, false, false, true, false);
-        midPointCircle({{startPoints[1].x - displacement, startPoints[1].y + displacement}, cornerRadious}, false, false, false, true);
+        drawCircle({{startPoints[1].x - displacement, startPoints[0].y - displacement}, cornerRadious}, true, false, false, false);
+        drawCircle({{startPoints[0].x + displacement, startPoints[0].y - displacement}, cornerRadious}, false, true, false, false);
+        drawCircle({{startPoints[0].x + displacement, startPoints[1].y + displacement}, cornerRadious}, false, false, true, false);
+        drawCircle({{startPoints[1].x - displacement, startPoints[1].y + displacement}, cornerRadious}, false, false, false, true);
     }
 
     glutSwapBuffers();
@@ -128,7 +163,7 @@ void mouseClicked(int button, int state, int x, int y) {
     y = HEIGHT - y;
     if (state == GLUT_UP) {
         if (!ready) {
-            startPoints[pointsReceived++] = {x, y};
+            startPoints[pointsReceived++] = {(double) x, (double) y};
             cornerRadious = (abs(startPoints[0].x - startPoints[1].x) / 2.0 + abs(startPoints[0].y - startPoints[1].y) / 2.0) / 5.0;
             if (pointsReceived == 2)
                 ready = true;
@@ -158,7 +193,7 @@ int main(int argc, char *argv[]) {
     //glutReshapeFunc(reshape);
     /* Callback para para interagir com teclado */
     //glutKeyboardFunc(keyboard);
-    printf((char*)glGetString(GL_VERSION));
+    printf("%s\n", glGetString(GL_VERSION));
     /* inicia o processamento dos eventos... */
     glutMainLoop();
 
